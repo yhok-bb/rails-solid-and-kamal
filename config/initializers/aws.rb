85e9cbd2d76f5dfc8c74a16d0c817992bc8db53a
yohei.hokari <yohei.hokari@gmail.com>
2026-06-06 16:03:46 +0900

    Fix AWS SDK checksum config for R2 compatibility

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,2 +1,5 @@
 require "aws-sdk-s3"
-Aws.config.update(http_checksum_validation: false)
+Aws.config.update(
+  request_checksum_calculation: "when_required",
+  response_checksum_validation: "when_required"
+)
