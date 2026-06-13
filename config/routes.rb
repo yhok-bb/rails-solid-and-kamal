b9a099d07128cc265f0d12195b188d67009a42ba
yohei.hokari <yohei.hokari@gmail.com>
2026-06-13 18:32:53 +0900

    Add Rails 8 authentication for distributed session experiments
    Uses built-in authentication generator (not Devise) to keep internals
    readable. Prepares for Phase 1 of distributed-plan: memory store session
    breakage on multi-server setup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
